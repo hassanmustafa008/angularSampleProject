bfb8c3b1aab99f57043a2accc1f68df11ff29527
hassanmustafa <hassan.mustafa@itboost.com>
2019-02-19 16:43:13 +0500

    Before routing concept.
    Signed-off-by: hassanmustafa <hassanmustafa008@gmail.com>

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {HeroService} from "../hero.service";
 import {Hero} from '../hero';
-import {HEROES} from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,16 +8,23 @@ import {HEROES} from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-  heros = HEROES;
+  heroes: Hero[];
+  selectedHero: Hero;
 
-  constructor() {
+  constructor(private heroService: HeroService) {
   }
 
   ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    debugger;
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
